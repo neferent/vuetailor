77ef3f8a0f33dc5a72dd22f0e4817eac8d30d11e
Anfal <muhammadanfal0@gmail.com>
2021-05-23 08:56:09 +0000

    Clean up the template

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,28 +1,13 @@
 <template>
     <Layout>
         <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-        <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-        <welcome></welcome>
-
-        <a
-            class="mt-auto bg-green-500 bg-opacity-50 hover:bg-opacity-75 transition-colors duration-200 rounded-xl font-semibold py-2 px-4 inline-flex"
-        >
-            Hello World
-        </a>
     </Layout>
 </template>
 
 <script>
-import Welcome from "~/components/welcome.vue";
-
 export default {
     metaInfo: {
         title: "Hello, world!"
-    },
-
-    components: {
-        Welcome
     }
 };
 </script>
