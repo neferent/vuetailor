f579f18027a59db9007d63c72cda9f1b91b7b745
Anfal <muhammadanfal0@gmail.com>
2021-05-26 10:14:19 +0000

    WIP

--- src/components/system/LeftNavbar.vue
+++ src/components/system/LeftNavbar.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="bg-gray-800">
-        test
+    <div class="bg-gray-800 flex flex-col">
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
     </div>
 </template>
